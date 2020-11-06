b5915dd3f53567a1aa84b6a82a2c333b3e4b5158
Norbert Melzer <timmelzer@gmail.com>
2020-11-06 07:38:34 +0100

    hello world

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
-  inputs.nixpkgs.url = "github:nixpkgs/nixos/nixos-20.09";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    devShell = nixpkgs.mkShell {
-      buildInputs = [ nipkgs.gnumake ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.meson pkgs.clang pkgs.ninja ];
+        };
+      });
 }
